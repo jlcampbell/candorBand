bed8f5d2f5ac3de562d036483f25c27b40e5ef73
jessica <Jess@jessicas-mbp.lan>
2021-11-19 23:20:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logoName.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="banner">
+      <div className='Nav'>
+        
+        <img className="logo-sm" src={logo} alt="Candor Logo"></img>
+        <ul className="navLinks">
+        
+        <li><a href="https://linktr.ee/CandorBand" target="_blank" rel="noopener noreferrer">
+          music
+          </a>
+          </li>
+
+        </ul>
+      </div>
+        {/* <img className="logo" src={logo}></img> */}
+
+      </div>
+      {/* <ul className="Highlights">
+        <li id="ep" className="Highlight">
+          <div className="ep-background"></div>
+          <div  className="ep-content">
+            {/* <h2>new ep</h2>
+            <span>Out now</span>
+            <button><a>Stream</a></button> */}
+          {/* </div> */}
+        {/* </li> */}
+      {/* </ul> */}
+     <footer>
+       <button>
+         <a>
+          follow us
+
+         </a>
+       </button>
+     <img className="logo-footer" src={logo} alt="Candor Logo"></img>
+       <div className='name'>©Candor, 2021</div>
+       <div className='builtBy'>site by jessica campbell</div>
+
+     </footer>
     </div>
   );
 }
